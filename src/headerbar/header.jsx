05f9ec19dc9e22666d88033e3878db583b0b02d2
Will <furtivespy@gmail.com>
2018-12-26 07:50:02 -0800

    user image

--- src/headerbar/header.jsx
+++ src/headerbar/header.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Navbar, NavbarGroup, NavbarDivider, NavbarHeading, Button, Alignment} from '@blueprintjs/core'
+import {Navbar, NavbarGroup, NavbarDivider, NavbarHeading, Button, Alignment, Icon} from '@blueprintjs/core'
 import { isLoaded, isEmpty } from 'react-redux-firebase'
 import {Link} from 'react-router-dom'
 
 const Title = styled.span`
     font-weight: bold;
     font-size: 26px;
+`
+
+const UserImage = styled.img`
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
 `
 
 class Header extends React.Component {
@@ -24,12 +30,12 @@ class Header extends React.Component {
                     <NavbarDivider />
                     <Link to="/login">
                         { !isLoaded(this.props.profile) || isEmpty(this.props.profile) ? 
-                          <Button minimal={true} icon="user"></Button>
-                        : <Button minimal={true}><img src={this.props.profile.avatarUrl} alt="user image" /></Button>
+                          <Button minimal={true}><Icon icon="user" iconSize={30}/></Button>
+                        : <Button minimal={true}><UserImage src={this.props.profile.avatarUrl} alt="user image" /></Button>
                         }
                     </Link>
-                    <Button minimal={true} icon="help"></Button>
-                    <Link to="/admin"><Button minimal={true} icon="cog"></Button></Link>
+                    <Button minimal={true}><Icon icon="help" iconSize={20}/></Button>
+                    <Link to="/admin"><Button minimal={true}><Icon icon="cog" iconSize={20}/></Button></Link>
                 </NavbarGroup>
             </Navbar>
         )
